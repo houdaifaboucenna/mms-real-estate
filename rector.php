f1cfb4665934589bc66dacdb16fa3be2a495aad6
HoudaifaBS <boucena214@gmail.com>
2026-01-17 23:23:20 +0100

    chore: add laravel rector for php best practices and refactoring

---
+++ rector.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Caching\ValueObject\Storage\FileCacheStorage;
+use Rector\Config\RectorConfig;
+use RectorLaravel\Set\LaravelSetProvider;
+
+return RectorConfig::configure()
+    ->withParallel(240, 2)
+    ->withSetProviders(LaravelSetProvider::class)
+    ->withComposerBased(laravel: true)
+    ->withCache(
+        cacheDirectory: '/tmp/rector',
+        cacheClass: FileCacheStorage::class,
+    )
+    ->withPaths([
+        __DIR__ . '/app',
+        __DIR__ . '/bootstrap/app.php',
+        __DIR__ . '/config',
+        __DIR__ . '/database',
+        __DIR__ . '/routes',
+        // __DIR__ . '/tests',
+    ]);
