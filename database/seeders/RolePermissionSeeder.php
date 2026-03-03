b1250dfd7d962718aaa03d560c5232e721ab2d01
HoudaifaBS <boucena214@gmail.com>
2026-03-03 09:45:40 +0100

    feat: Integrate RolePermissionSeeder into the main DatabaseSeeder and dynamically create permissions from enum cases.

--- database/seeders/RolePermissionSeeder.php
+++ database/seeders/RolePermissionSeeder.php
@@ -20,28 +20,9 @@ class RolePermissionSeeder extends Seeder
         $admin = Role::findOrCreate(enum_value(RoleEnum::ADMIN));
         $user = Role::findOrCreate(enum_value(RoleEnum::USER));
 
-        Permission::insert([
-            ['name' => enum_value(PermissionEnum::CREATE_ARTICLES), 'guard_name' => 'web'],
-            ['name' => enum_value(PermissionEnum::EDIT_ARTICLES), 'guard_name' => 'web'],
-            ['name' => enum_value(PermissionEnum::DELETE_ARTICLES), 'guard_name' => 'web'],
-            ['name' => enum_value(PermissionEnum::VIEW_ARTICLES), 'guard_name' => 'web'],
-            ['name' => enum_value(PermissionEnum::CREATE_COMMENTS), 'guard_name' => 'web'],
-            ['name' => enum_value(PermissionEnum::DELETE_COMMENTS), 'guard_name' => 'web'],
-            ['name' => enum_value(PermissionEnum::VIEW_COMMENTS), 'guard_name' => 'web'],
-            ['name' => enum_value(PermissionEnum::CREATE_ESTATES), 'guard_name' => 'web'],
-            ['name' => enum_value(PermissionEnum::EDIT_ESTATES), 'guard_name' => 'web'],
-            ['name' => enum_value(PermissionEnum::DELETE_ESTATES), 'guard_name' => 'web'],
-            ['name' => enum_value(PermissionEnum::VIEW_ESTATES), 'guard_name' => 'web'],
-            ['name' => enum_value(PermissionEnum::CREATE_CONTACTS), 'guard_name' => 'web'],
-            ['name' => enum_value(PermissionEnum::EDIT_CONTACTS), 'guard_name' => 'web'],
-            ['name' => enum_value(PermissionEnum::DELETE_CONTACTS), 'guard_name' => 'web'],
-            ['name' => enum_value(PermissionEnum::VIEW_CONTACTS), 'guard_name' => 'web'],
-            ['name' => enum_value(PermissionEnum::EDIT_PROFILES), 'guard_name' => 'web'],
-            ['name' => enum_value(PermissionEnum::VIEW_PROFILES), 'guard_name' => 'web'],
-            ['name' => enum_value(PermissionEnum::EDIT_SETTINGS), 'guard_name' => 'web'],
-            ['name' => enum_value(PermissionEnum::VIEW_SETTINGS), 'guard_name' => 'web'],
-            ['name' => enum_value(PermissionEnum::VIEW_OVERVIEW), 'guard_name' => 'web'],
-        ]);
+        foreach (PermissionEnum::cases() as $permission) {
+            Permission::findOrCreate($permission->value, 'web');
+        }
 
         $admin->syncPermissions(Permission::all());
     }
